c02ed675cba57aec50bb5a77e95bc9f816e1e0a9
Frederik Adamsen <adamsen9@gmail.com>
2020-02-09 23:27:18 +0100

    go.

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TodoRoutingModule } from './todo-routing.module';
 import { SharedModule } from '../shared/shared.module';
@@ -12,6 +11,6 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   imports: [CommonModule, TodoRoutingModule, SharedModule, MatSidenavModule],
-  declarations: [NavbarComponent, DashboardComponent, TodoItemComponent, TodoListComponent, TopnavComponent, SidenavComponent]
+  declarations: [DashboardComponent, TodoItemComponent, TodoListComponent, TopnavComponent, SidenavComponent]
 })
 export class TodosModule { }
